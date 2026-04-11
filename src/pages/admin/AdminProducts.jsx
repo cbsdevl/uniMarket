d661b7a4f82c558be00b32444eff4bfcca120fcd
cbsdevl <nizeyimanaplacide2@gmail.com>
2026-04-11 18:28:21 +0200

    Update AdminProducts.jsx

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -164,7 +164,7 @@ const AdminProducts = () => {
 
   return (
     <div className="flex min-h-screen bg-gray-50">
-      <AdminSidebar />
+
 
       <main className="flex-1 p-6 lg:p-8">
         {/* Header */}
